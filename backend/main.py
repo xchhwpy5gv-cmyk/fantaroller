3a1b6ca36af8d7610e1aa0f47cbc22148f11835a
xchhwpy5gv-cmyk <282599824+xchhwpy5gv-cmyk@users.noreply.github.com>
2026-05-10 18:33:00 +0200

    fix CORS per Vercel

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://fantaroller.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

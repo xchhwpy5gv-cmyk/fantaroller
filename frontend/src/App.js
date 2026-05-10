52846e202e878201bb3bbd3dc07545739c05ae4b
xchhwpy5gv-cmyk <282599824+xchhwpy5gv-cmyk@users.noreply.github.com>
2026-05-10 18:21:49 +0200

    frontend punta a backend onLine

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
-const API = "http://127.0.0.1:8000";
+const API = "https://fantaroller-api.onrender.com";
+
 
 function App() {
   const [token, setToken] = useState(null);
